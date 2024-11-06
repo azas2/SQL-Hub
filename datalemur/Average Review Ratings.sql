619567b8cb50108fbc55dda343fe52fcbd98b3e8
Ahmed Alaa Abdel Rahman <60736482+azas2@users.noreply.github.com>
2024-11-06 20:16:01 +0200

    Update Average Review Ratings.sql

--- datalemur/Average Review Ratings.sql
+++ datalemur/Average Review Ratings.sql
@@ -1,1 +1,5 @@
 
+SELECT  extract(month from submit_date) as mth ,  product_id as product , round(AVG(stars),2)as avg_stars
+from reviews 
+group by extract(month from submit_date) ,product_id 
+ORDER BY extract(month from submit_date) ASC , product_id asc
