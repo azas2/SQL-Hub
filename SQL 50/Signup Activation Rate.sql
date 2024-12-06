001c2db8630d97681b1528ffafa000bb334548e0
Ahmed Alaa Abdel Rahman <60736482+azas2@users.noreply.github.com>
2024-12-06 11:49:17 +0200

    Delete SQL 50/Signup Activation Rate.sql

--- SQL 50/Signup Activation Rate.sql
+++
@@ -1,9 +0,0 @@
-SELECT
- round(
-count(te.email_id)*1.0/ COUNT(distinct em.email_id) 
- ,2) as confirm_rate
- from emails as em
- left join texts as te
- ON em.email_id=te.email_id	
- AND te.signup_action = 'Confirmed';
- 
